154556e4cfc8533cae772d71e3ce71b94ea5b3f2
zwh <happyzhangwenhui@163.com>
2016-08-16 09:52:38 +0800

    add xml

--- mystore/src/main/java/com/mystore/business/action/RegisterAction.java
+++ mystore/src/main/java/com/mystore/business/action/RegisterAction.java
@@ -1,11 +1,16 @@
 package com.mystore.business.action;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 
+import com.jlc.mkt.sms.interfaceapi.service.SmsApiService;
+import com.jlc.mkt.sms.pojo.SmsEntranceMap;
 import com.mystore.business.core.PublicKeyMap;
 import com.mystore.business.core.RSAUtils;
 
@@ -18,9 +23,15 @@ public class RegisterAction  extends BaseAction{
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	@Autowired
+	private SmsApiService smsApiService;
+	
+	@Autowired
+	private RedisTemplate<String, Serializable> redisTemplate;
+	
 	private Map<String, Object> model;
 	
-	public String goRegister(){
+	public String goRegister() throws Exception{
 		
 		model = new HashMap<String, Object>();
 		
@@ -28,6 +39,12 @@ public class RegisterAction  extends BaseAction{
 		
 		model.put("exponent", publicKeyMap.getExponent());
 		model.put("modulus", publicKeyMap.getModulus());
+		
+		smsApiService.smsCheckCode("15011297739", "12345", null, SmsEntranceMap.WEBMOBILEREG);
+		
+		redisTemplate.opsForValue().set("15011297739", 1);
+		
+		System.out.println(redisTemplate.opsForValue().get("15011297739"));
 		
 		return "goRegister";
 		
